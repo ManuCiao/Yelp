075fba97c7a9b2e8037d2950bf40ec3330322265
cgulli <caitlin.gulliford@gmail.com>
2016-11-14 17:39:30 +0000

    created Restaurant model and migrated database

--- yelp_clone/spec/features/restaurants_feature_spec.rb
+++ yelp_clone/spec/features/restaurants_feature_spec.rb
@@ -8,4 +8,15 @@ feature 'restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'Waffle House')
+    end
+
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('Waffle House')
+      expect(page).not_to have_content('No restaurants yet')
+    end
+  end
 end
