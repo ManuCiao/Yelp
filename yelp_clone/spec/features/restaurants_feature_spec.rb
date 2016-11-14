5a4d757c932ec5511a69edc76a562be2ccf66420
cgulli <caitlin.gulliford@gmail.com>
2016-11-14 15:58:20 +0000

    set up testing gems and initial feature test

---
+++ yelp_clone/spec/features/restaurants_feature_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'restaurants' do
+  context 'no restaurants have been added' do
+    scenario 'should display a prompt to add a restaurant' do
+      visit '/restaurants'
+      expect(page).to have_content 'No restaurants yet'
+      expect(page).to have_link 'Add a restaurant'
+    end
+  end
+end
