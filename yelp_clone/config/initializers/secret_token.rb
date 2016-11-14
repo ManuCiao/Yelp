18cf67fc7d255452a260132f923b8d825bf16d25
Katy McCann <katy.alice.mccann@gmail.com>
2016-11-14 15:22:08 +0000

    Ran rails new -d postpresql -T

---
+++ yelp_clone/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YelpClone::Application.config.secret_key_base = 'a966dc46329f51194243de9e9fb05d93fcd6be2a92838ef8c59bdb76e37ac7a33162bfeeca1ac2a3d942d44adf3f82d0b28d191359006c07c797796054f5acc2'
