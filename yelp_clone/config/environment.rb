076fde3c20c9273a19b1b474c597c9c489a4c486
Katy McCann <katy.alice.mccann@gmail.com>
2016-11-14 16:33:59 +0000

    Rails initalization configuration

--- yelp_clone/config/environment.rb
+++ yelp_clone/config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-YelpClone::Application.initialize!
+Rails.application.initialize!
--- yelp_clone/config/routes.rb
+++ yelp_clone/config/routes.rb
@@ -1,4 +1,4 @@
-YelpClone::Application.routes.draw do
+Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +39,7 @@ YelpClone::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
