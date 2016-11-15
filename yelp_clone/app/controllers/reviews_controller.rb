d66106fadcf014a65dbb3041a37f5552013cd603
ManuCiao <mn.sabatino@gmail.com>
2016-11-15 19:10:15 +0000

    tests passing add the review in the index file

--- yelp_clone/app/controllers/reviews_controller.rb
+++ yelp_clone/app/controllers/reviews_controller.rb
@@ -7,7 +7,8 @@ class ReviewsController < ApplicationController
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @restaurant.reviews.create(review_params)
+    @review = @restaurant.reviews.create(review_params)
+    redirect_to '/restaurants'
   end
 
 private
