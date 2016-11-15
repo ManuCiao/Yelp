b8f9860af8dbc1450e68b6805ea41228b3e38799
Stuart Cooper <stucooper86@gmail.com>
2016-11-15 17:17:47 +0000

    adding controller params

--- yelp_clone/app/controllers/restaurants_controller.rb
+++ yelp_clone/app/controllers/restaurants_controller.rb
@@ -1,8 +1,22 @@
 class RestaurantsController < ApplicationController
+
    def index
      @restaurants = Restaurant.all
    end
 
-   def new 
+   def new
+     @restaurant = Restaurant.new
    end
+
+   def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
 end
