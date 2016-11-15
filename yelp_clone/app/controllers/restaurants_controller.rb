a36f04533beae3c66c8e98fb2549a94ea561349d
Katy McCann <katy.alice.mccann@gmail.com>
2016-11-15 10:38:45 +0000

    Add controller method

--- yelp_clone/app/controllers/restaurants_controller.rb
+++ yelp_clone/app/controllers/restaurants_controller.rb
@@ -1,2 +1,5 @@
 class RestaurantsController < ApplicationController
+  def index
+    raise 'Hello from the index action'
+  end 
 end
--- yelp_clone/config/routes.rb
+++ yelp_clone/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'restaurants' => 'restaurants#index'
 
   resources :restaurants
   # The priority is based upon order of creation: first created -> highest priority.
