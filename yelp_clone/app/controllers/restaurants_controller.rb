3d3878d3b332f450c80b23f5584674e51574b685
Laura Weston <laura.weston@outlook.com>
2016-11-17 16:58:59 +0000

    User must be logged in to create, edit and delete restaurants

--- yelp_clone/app/controllers/restaurants_controller.rb
+++ yelp_clone/app/controllers/restaurants_controller.rb
@@ -1,5 +1,7 @@
 class RestaurantsController < ApplicationController
 
+  before_action :authenticate_user!, :except => [:index, :show]
+
   def index
     @restaurants = Restaurant.all
   end
--- yelp_clone/spec/features/restaurants_features_spec.rb
+++ yelp_clone/spec/features/restaurants_features_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require_relative 'sign_in_helper'
 
 feature 'restaurants' do
   context 'no restaurants have been added' do
@@ -22,23 +23,34 @@ feature 'restaurants' do
   end
 
   context 'creating restaurant' do
-    scenario 'user fills out a form and adds restaurant' do
-      visit '/restaurants'
-      click_link 'Add a restaurant'
-      fill_in 'Name', with: 'Pret'
-      click_button 'Create Restaurant'
-      expect(page).to have_content('Pret')
-      expect(current_path).to eq '/restaurants'
-    end
-
-    context 'an invalid restaurant' do
-      scenario 'does not let you submit a name that is too short' do
+    context 'user not signed in' do
+      scenario 'user cannot add a new restaurant' do
         visit '/restaurants'
         click_link 'Add a restaurant'
-        fill_in 'Name', with: 'kf'
+        expect(page).not_to have_css 'h2', text: 'Pret'
+        expect(page).to have_content 'You need to sign in or sign up before continuing'
+      end
+    end
+
+    context 'user signed in' do
+      scenario 'user fills out a form and adds restaurant' do
+        sign_in
+        click_link 'Add a restaurant'
+        fill_in 'Name', with: 'Pret'
         click_button 'Create Restaurant'
-        expect(page).not_to have_css 'h2', text: 'kf'
-        expect(page).to have_content 'error'
+        expect(page).to have_content('Pret')
+        expect(current_path).to eq '/restaurants'
+      end
+
+      context 'an invalid restaurant' do
+        scenario 'does not let you submit a name that is too short' do
+          sign_in
+          click_link 'Add a restaurant'
+          fill_in 'Name', with: 'kf'
+          click_button 'Create Restaurant'
+          expect(page).not_to have_css 'h2', text: 'kf'
+          expect(page).to have_content 'error'
+        end
       end
     end
   end
@@ -60,7 +72,7 @@ feature 'restaurants' do
     before { Restaurant.create name: 'Pret', description: 'Sandwiches' }
 
     scenario 'let a user edit a restaurant' do
-      visit '/restaurants'
+      sign_in
       click_link 'Edit Pret'
       fill_in 'Name', with: 'Pret A Manger'
       fill_in 'Description', with: 'Tasty Sandwiches'
@@ -74,8 +86,8 @@ feature 'restaurants' do
   context 'deleting restaurants' do
     before { Restaurant.create name: 'KFC', description: 'Deep fried goodness'}
 
-    scenario 'removes a restaurant when a user clicks  a delete link' do
-      visit '/restaurants'
+    scenario 'removes a restaurant when a user clicks a delete link' do
+      sign_in
       click_link 'Delete KFC'
       expect(page).not_to have_content 'KFC'
       expect(page).to have_content 'Restaurant deleted successfully'
