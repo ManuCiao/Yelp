f69b097ac29d9c35c36eb41f256f068b091ed30c
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-11-17 15:34:17 +0000

    added facebook login ability

--- yelp_clone/db/schema.rb
+++ yelp_clone/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117144801) do
+ActiveRecord::Schema.define(version: 20161117152339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20161117144801) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
