5c79ccd0d72a184497a90e86ccc1bf9ca7ea5a1d
ManuCiao <mn.sabatino@gmail.com>
2016-11-18 15:36:09 +0000

    add multiple ratings

--- yelp_clone/app/models/restaurant.rb
+++ yelp_clone/app/models/restaurant.rb
@@ -1,9 +1,13 @@
 class Restaurant < ActiveRecord::Base
+    belongs_to :user
+    validates :name, length: { minimum: 3 }, uniqueness: true
+    has_many :reviews,
+             -> { extending WithUserAssociationExtension },
+             dependent: :destroy
 
-  belongs_to :user
-  validates :name, length: { minimum: 3 }, uniqueness: true
-  has_many :reviews,
-      -> {extending WithUserAssociationExtension },
-      dependent: :destroy
-
+    def average_rating
+        return 'N/A' if reviews.none?
+        # reviews.inject(0) { |memo, review| memo + review.rating } / reviews.count
+        reviews.average(:rating)
+  end
 end
--- yelp_clone/spec/features/reviews_features_spec.rb
+++ yelp_clone/spec/features/reviews_features_spec.rb
@@ -1,16 +1,34 @@
 require 'rails_helper'
+require_relative 'sign_in_helper'
 
 feature 'reviewing' do
-  before { Restaurant.create name: 'KFC'}
+    before { Restaurant.create name: 'KFC' }
 
-  scenario 'allows users to leave a review using a forum' do
-    visit '/restaurants'
-    click_link 'Review KFC'
-    fill_in 'Thoughts', with: 'so so'
-    select '3', from: 'Rating'
-    click_button 'Leave Review'
+    scenario 'allows users to leave a review using a forum' do
+        visit '/restaurants'
+        click_link 'Review KFC'
+        fill_in 'Thoughts', with: 'so so'
+        select '3', from: 'Rating'
+        click_button 'Leave Review'
 
-    expect(current_path).to eq '/restaurants'
-    expect(page).to have_content('so so')
-  end 
+        expect(current_path).to eq '/restaurants'
+        expect(page).to have_content('so so')
+    end
+end
+
+feature 'reviewing' do
+    before { Restaurant.create name: 'KFC' }
+    def leave_review(thoughts, rating)
+        visit '/restaurants'
+        click_link 'Review KFC'
+        fill_in 'Thoughts', with: thoughts
+        select rating, from: 'Rating'
+        click_button 'Leave Review'
+    end
+
+    scenario 'displays an average rating for all reviews' do
+        leave_review('So so', '3')
+        leave_review('Great', '5')
+        expect(page).to have_content('Average rating: 4')
+    end
 end
