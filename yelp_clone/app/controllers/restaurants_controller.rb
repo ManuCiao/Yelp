ab5ce3ae6e70b1963706bb4a8cc8d9f686885119
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-11-18 16:58:01 +0000

    added image to restuarant

--- yelp_clone/app/controllers/restaurants_controller.rb
+++ yelp_clone/app/controllers/restaurants_controller.rb
@@ -44,7 +44,7 @@ class RestaurantsController < ApplicationController
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :description)
+    params.require(:restaurant).permit(:name, :description, :image)
   end
 
 end
