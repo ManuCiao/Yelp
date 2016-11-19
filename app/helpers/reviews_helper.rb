c4415ecbc807279f2a722b163f8e551af3751919
ManuCiao <mn.sabatino@gmail.com>
2016-11-19 16:26:48 +0000

    add stars to the rating

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,11 @@
 module ReviewsHelper
-end
+   def star_rating(rating)
+     # does the rating respond to `round`? If not, just return the rating
+     return rating unless rating.respond_to?(:round)
+
+     # if it does – i.e. if it's a valid number - then the rest of the
+     # method is run
+     remainder = (5 - rating.round)
+     "★" * rating.round + "☆" * remainder
+   end
+ end
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -28,6 +28,6 @@ feature 'reviewing' do
     scenario 'displays an average rating for all reviews' do
         leave_review('So so', '3')
         leave_review('Great', '5')
-        expect(page).to have_content('Average rating: 4')
+        expect(page).to have_content('Average rating: ★★★★☆')
     end
 end
